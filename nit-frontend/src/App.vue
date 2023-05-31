62da155b64191374786d596f4e9b5f65ade677f8
taocyber <2450924006@qq.com>
2023-05-31 16:12:34 +0800

    前端：用户登陆界设计

--- nit-frontend/src/App.vue
+++ nit-frontend/src/App.vue
@@ -1,85 +1,88 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup >
+import {User,Lock} from '@element-plus/icons-vue'
+
+
+
 </script>
 
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<!--  图片展示区域-->
+  <div style="width: 100vw;height: 100vw;overflow: hidden;display: flex">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div style="flex: 1">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <img  style="width: 100%;height: 100%;" fit ="cover"
+            src="@/assets/Picture/login1.png" alt="自由才是这个世界的第一要义"/>
+
     </div>
-  </header>
+<!--    登录区域界面设计-->
+    <div style="width: 400px;background-color: #d7fafa">
 
-  <RouterView />
+      <div style="text-align: center; margin: 0 20px">
+        <div style="margin-top:150px">
+          <div style="font-size: 25px">登录</div>
+          <div style="font-size: 14px;color: black">在进入系统前请先输入用户名和密码进行登录</div>
+        </div>
+<!--       登录行设计-->
+        <div style="margin-top: 30px;font-size: 14px" >
+<!--          用户账号/邮箱输入区-->
+          <el-input type="text" placeholder="用户名/邮箱">
+            <template #prefix>
+              <el-icon slot="prefix"><User /></el-icon>
+            </template>
+          </el-input>
+<!--          用户密码输入区-->
+          <el-input type="password" style="margin-top: 4px" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </div>
+<!--        小项功能：记住密码和账号，忘记密码连接-->
+        <div >
+<!--          自动记住账号密码-->
+            <el-row style="margin-top: 5px;font-size:14px">
+              <el-col :span="12" style="text-align: left">
+                <el-checkbox v-model="checked1" label="记住我"/>
+              </el-col>
+<!--           忘记密码请求-->
+              <el-col :span="12" style="text-align: right">
+                <el-link>忘记密码</el-link>
+
+              </el-col>
+            </el-row>
+
+        </div>
+<!--        按钮区域-->
+        <div style="margin-top: 40px" >
+          <el-button style="width: 150px" type="success" color="#00FF7F" plain>立即登录</el-button>
+        </div>
+          <el-divider>
+            <span style="color:#DC143C ">没有账号</span>
+          </el-divider>
+
+        <div>
+          <el-button style="width: 150px" type="primary" color="#FF8C00" plain>注册账号</el-button>
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+
 </template>
 
+
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
